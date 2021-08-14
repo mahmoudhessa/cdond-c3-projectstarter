c5775202a10260034d860032a1d1fd2ea65d545e
Mahmoud Abuelella <SESA574153@se.com>
2021-08-14 03:20:02 +0200

    Vulnerable job

--- backend/src/modules/domain/employees/commands/handlers/employee-address-updater.handler.spec.ts
+++ backend/src/modules/domain/employees/commands/handlers/employee-address-updater.handler.spec.ts
@@ -19,7 +19,7 @@ describe('Employee Address Updater', () => {
       const handler = new EmployeeAddressUpdater(employeeRepository);
 
       const params = {
-        employeeId: 101,
+        employeeId: 100,
         address: 'San Pedro Sula, Calle 1, Casa 5',
         city: 'San Pedro Sula',
         country: 'Honduras',
